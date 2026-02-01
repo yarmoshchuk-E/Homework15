4467e67c85ca334914aec18d2a7be7daf1cc4283
yarmoshchuk-E <yarmoshchuk_80@mail.ru>
2026-02-01 20:01:46 +0400

    отредактировал по пунктам

--- src/Order.java
+++ src/Order.java
@@ -32,26 +32,46 @@ public class Order {
         return "Покупатель: " + getCustomer() + " " + Arrays.toString(basket);
     }
 
-
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
+        if (this == o) {
+            return true;
+        }
         if (o == null || getClass() != o.getClass()) {
             return false;
         }
         Order order = (Order) o;
-
-        if (basket == null || order.basket == null) {
-            return false;
-        }
-        if (basket.length == order.basket.length) {
+        //Проверку Objects.equals(customer, order.customer) - поставить на первое место, перед проверкой на null basket'ов.
+        if (Objects.equals(customer, order.customer)) {
             return true;
         }
+        //Тебе следует проверить на НЕ совпадение длины массивов.
+        if (basket.length != order.basket.length) {
+            return false;
+        }
+        //Добавь проверку, если оба basket равны нулю - вернуть истину
+        if (basket == null && order.basket == null) {
+            return true;
+        }
+
         for (int i = 0; i < basket.length; i++) {
-            if (basket[i] != order.basket[i]) {
+            //Первая проверка - если элементы массива равны нулю оба, следует пропустить итерацию
+            //(в массивах первые элементы отсутствуют, к примеру).
+            if (basket[i] == null && order.basket[i] == null) {
+                return true;                                        //
+            }
+            //Вторая проверка - если один из элементов равен null, а другой не равен null.
+            if (basket[i] == null && order.basket[i] != null || basket[i] != null && order.basket[i] == null) {
                 return false;
             }
+            //Только после этого, тебе требуется проверить элементы не через == (как у тебя),
+            //а через equals (p1.equals(p2)) т.к. это объекты.
+            if (basket.equals(order.basket)) {
+                return true;
+            }
         }
-        return Objects.equals(customer, order.customer) && Arrays.equals(basket, order.basket);
+        //Проверку Arrays.equals(basket, order.basket) - требуется убрать.
+        //Вместо этих проверок, верни просто true: return true.
+        return true;
     }
 }
